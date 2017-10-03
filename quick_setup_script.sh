0a46c003e7b48d06f738e02706e21daf2ba6bd9f
brianbreniser <brianbreniser@gmail.com>
2017-10-02 19:31:08 -0700

    added some cargo things, updated main list

--- quick_setup_script.sh
+++ quick_setup_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # fail on anything erroring
 
 # What OS is this? Just ask the user, it's easier
 echo "***INFORM*** Checking Operating system..."
@@ -18,11 +19,18 @@ case $1 in
         ;;
 esac
 
+# to install cargo things if rust gets installed
+cargo_install="cargo install "
+
 echo "***INFORM*** Oh, we are on $1"
 echo "Lets install some things..."
 install_list=(
               fish # fish shell because awesomeness
               htop # This isn't on some distrobutions for some reason, so usefull
+              ranger # awesome folder viewer
+              rofi # launcher
+              vim # not always installed
+              # nvim # nvim >> vim but will this actually install it?
              )
 
 # Installer section
@@ -57,6 +65,40 @@ for i in "${universal_installer_commands[@]}"
         eval $i # AHHHHH INSTALL SECTION BE SO CAREFUL HERE I'M SERIOUS -----------------------------------------
     done
 
+# Rust specific things
+echo "***INFORM*** installing some awesome rust things"
+cargo_install_list=(
+                   ripgrep
+                   "fd-find"
+                   exa
+                  )
+
+# Rust installer section
+for i in ${cargo_install_list[@]}
+    do
+        eval "echo $cargo_install $i" # AHHHHH INSTALL SECTION BE SO CAREFUL HERE I'M SERIOUS -----------------------------------------
+    done
+
+
 done_installing="***INFORM*** done installing things"
 eval "echo $done_installing" # yikes that's pretty ugly
+
+### Some notes on things I don't know if I can automate ###
+## Sublime text 3 packages
+# anaconda_rust - for sublime racer/rls things
+# colorsublime - Themes
+# dateutil
+# plaintasks
+# pyyaml
+# theme -e default
+# sulimeCodeIntel
+# backrefs
+# markupsafe
+# mdpopups
+# pygments
+# python-jinja2
+# python-markdown
+# pyyaml
+# SublimeLinter
+
 
